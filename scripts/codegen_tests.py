e7f699fb86a6034086db1700fc858307e613619c
zl8115 <ze.rong96@gmail.com>
2025-10-04 14:37:06 +0100

    Rework test script to take arguments
    This removes the requirement of an env var
    
    and also allows users to reduce the number of tests to generate

--- scripts/codegen_tests.py
+++ scripts/codegen_tests.py
@@ -1,13 +1,11 @@
-import os
 import pathlib
 import json
 import re
+import random
+import argparse
+from typing import Optional
 from string import Template
 
-# https://github.com/SingleStepTests/sm83
-REPO_PATH = os.getenv("SM83TestsPath")
-assert REPO_PATH, "REPO_PATH cannot be empty"
-TEST_DIR = os.path.join(REPO_PATH, "v1")
 CPP_HEADER_TEMPLATE = Template("""// Based on https://github.com/SingleStepTests/sm83
 // File: $test_file_name
 #include <catch2/catch_test_macros.hpp>
@@ -52,9 +50,22 @@ $expected_ram_lines
 }
 """)
 
-test_files = filter(lambda p: re.search(r'^([0-9a-f]|cb [0-2])[0-9a-f].json', str(p.name)), pathlib.Path(TEST_DIR).iterdir())
-for test_file in test_files:
+def generate_tests(test_dir: pathlib.Path, tests_count: Optional[int]) -> None:
+    # The test json files should match the pattern like '0f.json' or 'cb 9a.json'
+    TEST_FILE_REGEX = re.compile(r'^([0-9a-f]|cb [0-2])[0-9a-f]\.json')
+
+    test_files = filter(lambda p: TEST_FILE_REGEX.search(str(p.name)), pathlib.Path(test_dir).iterdir())
+    for test_file in test_files:
+        generate_test(test_file, tests_count)
+
+def generate_test(test_file: pathlib.Path, tests_count: Optional[int]) -> None:
+    # Load Json File
     test_json = json.loads(test_file.read_text());
+    if ((tests_count is not None) and (tests_count > 0)):
+        random.seed(83)
+        test_json = random.sample(test_json, min(tests_count, len(test_json)))
+
+    # Write output .cpp header
     out_file_name = test_file.stem + ".cpp"
     out_dir = pathlib.Path(__file__).parent.parent / "src" / "sm83" / "test"
     out_file = open(out_dir / out_file_name, "w")
@@ -105,3 +116,36 @@ for test_file in test_files:
             expected_ram_lines = expected_ram_lines,
         ))
     out_file.close()
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        prog='codegen_tests.py',
+        description='''
+            Script to codegen the SM83 C++ unittests based on test json files from https://github.com/SingleStepTests/sm83.
+
+            Example:
+                codegen_tests.py ~/src/sm83/v1/ --test_count 100
+        ''')
+    parser.add_argument(
+        'test_dir',
+        help='Directory to the SM83 v1 tests (https://github.com/SingleStepTests/sm83). E.g. `~/src/sm83/v1/`',
+        nargs='?',
+        const=None,
+        type=pathlib.Path)
+    parser.add_argument(
+        '-c',
+        '--test_count',
+        help='Number of tests per Test Case (opcode) to add. Each opcode has 1000 test cases which will make compilation slow. So you can reduce this to compile less. If None is specified, all test cases will be generated. Default is None.',
+        nargs='?',
+        const=None,
+        type=int)
+
+    args = parser.parse_args()
+    return args
+
+def main() -> None:
+    args = parse_args()
+    generate_tests(args.test_dir, args.test_count)
+
+if __name__ == "__main__":
+    main()
